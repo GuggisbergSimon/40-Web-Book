14b3f5552d4a1449a3463350df723e1e1f011379
Ataor <slyguggi@gmail.com>
2021-01-01 13:24:12 +0100

    fixed landing page

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * Authors : Julien Leresche & Simon Guggisberg
+ * Date : 01.01.2021
+ * Description : Landing page redirecting to the correct php file
+ */
+
+header("Location: controller/home.php");
