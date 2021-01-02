1ff13fc02c32ed1f6eb9c60d5d59c7f1df218fcf
Ataor <slyguggi@gmail.com>
2021-01-02 12:47:38 +0100

    major cleanup/format

--- controller/addRating.php
+++ controller/addRating.php
@@ -14,36 +14,28 @@ session_start();
 
 $users = $database->getTable("t_user");
 $evaluations = $database->getAllEvaluationsFromBook($_GET["idBook"]);
-$alreadyRated=0;
+$alreadyRated = 0;
 $activeUser;
-foreach($users as $user)
-{
-    if($user["usePseudo"] == $_SESSION["username"])
-    {
+foreach ($users as $user) {
+    if ($user["usePseudo"] == $_SESSION["username"]) {
         $activeUser = $user;
     }
 }
-foreach($evaluations as $evaluation)
-{
-    if($evaluation["idUserEvaluer"] == $activeUser["idUser"])
-    {
-        $alreadyRated=1;
+foreach ($evaluations as $evaluation) {
+    if ($evaluation["idUserEvaluer"] == $activeUser["idUser"]) {
+        $alreadyRated = 1;
     }
 }
 
-if(isset($_POST["addRating"]))
-{
-    if($alreadyRated==0)
-    {
-        $database->addRating($_GET["idBook"],$activeUser["idUser"],$_POST["note"],$_POST["summary"]);
-    } 
-    else 
-    {
-        $database->updateRating($_GET["idBook"],$activeUser["idUser"],$_POST["note"],$_POST["summary"]);
+if (isset($_POST["addRating"])) {
+    if ($alreadyRated == 0) {
+        $database->addRating($_GET["idBook"], $activeUser["idUser"], $_POST["note"], $_POST["summary"]);
+    } else {
+        $database->updateRating($_GET["idBook"], $activeUser["idUser"], $_POST["note"], $_POST["summary"]);
     }
     $evaluations = $database->getAllEvaluationsFromBook($_GET["idBook"]);
-    $database->modifyBookAverageNote(computeAverageNote($evaluations),$_GET["idBook"]);
-} 
+    $database->modifyBookAverageNote(computeAverageNote($evaluations), $_GET["idBook"]);
+}
 
 header("location: rating.php?idBook=" . $_GET["idBook"])
 ?>
